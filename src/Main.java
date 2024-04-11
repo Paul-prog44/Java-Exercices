79050cd35b602f869ed552a83f1bdbb94e72d4aa
Paul-prog44 <saintouenfr@hotmail.com>
2024-04-11 17:12:58 +0200

    Add regex ex

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		BookComparator bc = new BookComparator();
+		Scanner scanner = new Scanner(System.in);
+		Checker checker = new Checker();
 		
-		bc.compareBooks();
+		while (true) {
+			System.out.print("Enter a string: ");
+			String input = scanner.nextLine();
+			
+			if (checker.timeOfDay(input)) {
+				System.out.println("The form is correct");
+			} else {
+				System.out.println("The form is incorrect");
+			}
+			
+			
+			
+		}
+		
 	}
 	
 }
